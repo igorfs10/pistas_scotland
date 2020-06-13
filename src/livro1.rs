71347afcbdcfcd9e3f1cf94ceb20cd06cb77ef3c
Igorfs10 <igorfs10@gmail.com>
2020-06-13 19:03:00 -0300

    adicionar mais pistas e usar constantes

--- src/livro1.rs
+++ src/livro1.rs
@@ -1,3 +1,12 @@
-pub const LIVRO_1: [&str; 1] = [
-    "Earl Akintern tem constantes dores de cabeça."
+const SEM_PISTA: &str = "(Sem pista.)";
+
+pub const LIVRO_1: [&str; 8] = [
+    "Earl Akintern tem constantes dores de cabeça.",
+    "A faca no peito de Foster não foi enfiada por mãos humanas.",
+    "Pista do assassino (3 partes): III) Sabor amargo.",
+    "O chão da baia de Mecha Prateada tinha pingos frescos de tinta preta.",
+    "Pista do motivo (4 partes): III) Digo.",
+    "Coronel Sebastian Moran",
+    SEM_PISTA,
+    "Jules Redmarch comprou algumas pílulas para atenuar suas vertigens"
 ];
