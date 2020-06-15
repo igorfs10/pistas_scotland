0d9716ccf41686a556528b6e69ffd0565c6ee303
Igorfs10 <igorfs10@gmail.com>
2020-06-15 00:39:56 -0300

    remove o áudio para colocar dentro do código,
    limpeza de código e removido js para fazer tudo em Rust

--- pkg/pistas_scotland.js
+++ pkg/pistas_scotland.js
@@ -30,9 +30,72 @@ function addHeapObject(obj) {
     return idx;
 }
 
-let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+function debugString(val) {
+    // primitive types
+    const type = typeof val;
+    if (type == 'number' || type == 'boolean' || val == null) {
+        return  `${val}`;
+    }
+    if (type == 'string') {
+        return `"${val}"`;
+    }
+    if (type == 'symbol') {
+        const description = val.description;
+        if (description == null) {
+            return 'Symbol';
+        } else {
+            return `Symbol(${description})`;
+        }
+    }
+    if (type == 'function') {
+        const name = val.name;
+        if (typeof name == 'string' && name.length > 0) {
+            return `Function(${name})`;
+        } else {
+            return 'Function';
+        }
+    }
+    // objects
+    if (Array.isArray(val)) {
+        const length = val.length;
+        let debug = '[';
+        if (length > 0) {
+            debug += debugString(val[0]);
+        }
+        for(let i = 1; i < length; i++) {
+            debug += ', ' + debugString(val[i]);
+        }
+        debug += ']';
+        return debug;
+    }
+    // Test for built-in
+    const builtInMatches = /\[object ([^\]]+)\]/.exec(toString.call(val));
+    let className;
+    if (builtInMatches.length > 1) {
+        className = builtInMatches[1];
+    } else {
+        // Failed to match the standard '[object ClassName]'
+        return toString.call(val);
+    }
+    if (className == 'Object') {
+        // we're a user defined class or Object
+        // JSON.stringify avoids problems with cycles, and is generally much
+        // easier than looping through ownProperties of `val`.
+        try {
+            return 'Object(' + JSON.stringify(val) + ')';
+        } catch (_) {
+            return 'Object';
+        }
+    }
+    // errors
+    if (val instanceof Error) {
+        return `${val.name}: ${val.message}\n${val.stack}`;
+    }
+    // TODO we could test for more things here, like `Set`s and `Map`s.
+    return className;
+}
 
-cachedTextDecoder.decode();
+let WASM_VECTOR_LEN = 0;
 
 let cachegetUint8Memory0 = null;
 function getUint8Memory0() {
@@ -42,28 +105,102 @@ function getUint8Memory0() {
     return cachegetUint8Memory0;
 }
 
+let cachedTextEncoder = new TextEncoder('utf-8');
+
+const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+    ? function (arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+}
+    : function (arg, view) {
+    const buf = cachedTextEncoder.encode(arg);
+    view.set(buf);
+    return {
+        read: arg.length,
+        written: buf.length
+    };
+});
+
+function passStringToWasm0(arg, malloc, realloc) {
+
+    if (realloc === undefined) {
+        const buf = cachedTextEncoder.encode(arg);
+        const ptr = malloc(buf.length);
+        getUint8Memory0().subarray(ptr, ptr + buf.length).set(buf);
+        WASM_VECTOR_LEN = buf.length;
+        return ptr;
+    }
+
+    let len = arg.length;
+    let ptr = malloc(len);
+
+    const mem = getUint8Memory0();
+
+    let offset = 0;
+
+    for (; offset < len; offset++) {
+        const code = arg.charCodeAt(offset);
+        if (code > 0x7F) break;
+        mem[ptr + offset] = code;
+    }
+
+    if (offset !== len) {
+        if (offset !== 0) {
+            arg = arg.slice(offset);
+        }
+        ptr = realloc(ptr, len, len = offset + arg.length * 3);
+        const view = getUint8Memory0().subarray(ptr + offset, ptr + len);
+        const ret = encodeString(arg, view);
+
+        offset += ret.written;
+    }
+
+    WASM_VECTOR_LEN = offset;
+    return ptr;
+}
+
+let cachegetInt32Memory0 = null;
+function getInt32Memory0() {
+    if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
+        cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    }
+    return cachegetInt32Memory0;
+}
+
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
 function getStringFromWasm0(ptr, len) {
     return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
 }
+
+function makeMutClosure(arg0, arg1, dtor, f) {
+    const state = { a: arg0, b: arg1, cnt: 1 };
+    const real = (...args) => {
+        // First up with a closure we increment the internal reference
+        // count. This ensures that the Rust closure environment won't
+        // be deallocated while we're invoking it.
+        state.cnt++;
+        const a = state.a;
+        state.a = 0;
+        try {
+            return f(a, state.b, ...args);
+        } finally {
+            if (--state.cnt === 0) wasm.__wbindgen_export_2.get(dtor)(a, state.b);
+            else state.a = a;
+        }
+    };
+    real.original = state;
+    return real;
+}
+function __wbg_adapter_18(arg0, arg1) {
+    wasm._dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5c82c74a0426567a(arg0, arg1);
+}
+
 /**
 */
 export function main() {
     wasm.main();
-}
-
-/**
-* @param {number} numero_livro
-* @param {number} numero_pista
-*/
-export function usar_pista(numero_livro, numero_pista) {
-    wasm.usar_pista(numero_livro, numero_pista);
-}
-
-/**
-* @param {number} tempo_restante
-*/
-export function atualizar_tempo_restante(tempo_restante) {
-    wasm.atualizar_tempo_restante(tempo_restante);
 }
 
 function isLikeNone(x) {
@@ -120,6 +257,18 @@ async function init(input) {
     }
     const imports = {};
     imports.wbg = {};
+    imports.wbg.__wbindgen_cb_forget = function(arg0) {
+        takeObject(arg0);
+    };
+    imports.wbg.__wbindgen_cb_drop = function(arg0) {
+        const obj = takeObject(arg0).original;
+        if (obj.cnt-- == 1) {
+            obj.a = 0;
+            return true;
+        }
+        var ret = false;
+        return ret;
+    };
     imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
         takeObject(arg0);
     };
@@ -131,6 +280,13 @@ async function init(input) {
         var ret = getObject(arg0).document;
         return isLikeNone(ret) ? 0 : addHeapObject(ret);
     };
+    imports.wbg.__wbg_alert_8e01aaa8c481629b = handleError(function(arg0, arg1, arg2) {
+        getObject(arg0).alert(getStringFromWasm0(arg1, arg2));
+    });
+    imports.wbg.__wbg_setTimeout_793b8ce420982f13 = handleError(function(arg0, arg1, arg2) {
+        var ret = getObject(arg0).setTimeout(getObject(arg1), arg2);
+        return ret;
+    });
     imports.wbg.__wbg_getElementById_0e0dd6444cdc0602 = function(arg0, arg1, arg2) {
         var ret = getObject(arg0).getElementById(getStringFromWasm0(arg1, arg2));
         return isLikeNone(ret) ? 0 : addHeapObject(ret);
@@ -139,12 +295,45 @@ async function init(input) {
         var ret = getObject(arg0) instanceof HTMLElement;
         return ret;
     };
+    imports.wbg.__wbg_innerText_e3b6f8b79dd96b54 = function(arg0, arg1) {
+        var ret = getObject(arg1).innerText;
+        var ptr0 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len0 = WASM_VECTOR_LEN;
+        getInt32Memory0()[arg0 / 4 + 1] = len0;
+        getInt32Memory0()[arg0 / 4 + 0] = ptr0;
+    };
     imports.wbg.__wbg_setinnerText_17717ee9be6024fe = function(arg0, arg1, arg2) {
         getObject(arg0).innerText = getStringFromWasm0(arg1, arg2);
     };
     imports.wbg.__wbg_sethidden_61fc5d58baaf5dce = function(arg0, arg1) {
         getObject(arg0).hidden = arg1 !== 0;
     };
+    imports.wbg.__wbg_setonclick_5ec75538316eafe0 = function(arg0, arg1) {
+        getObject(arg0).onclick = getObject(arg1);
+    };
+    imports.wbg.__wbg_instanceof_HtmlInputElement_a568dc824e934b93 = function(arg0) {
+        var ret = getObject(arg0) instanceof HTMLInputElement;
+        return ret;
+    };
+    imports.wbg.__wbg_checked_2885cce47fd9e289 = function(arg0) {
+        var ret = getObject(arg0).checked;
+        return ret;
+    };
+    imports.wbg.__wbg_value_d00c8cb8abcfbeeb = function(arg0, arg1) {
+        var ret = getObject(arg1).value;
+        var ptr0 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len0 = WASM_VECTOR_LEN;
+        getInt32Memory0()[arg0 / 4 + 1] = len0;
+        getInt32Memory0()[arg0 / 4 + 0] = ptr0;
+    };
+    imports.wbg.__wbg_play_3741fe4b5f5c3244 = handleError(function(arg0) {
+        var ret = getObject(arg0).play();
+        return addHeapObject(ret);
+    });
+    imports.wbg.__wbg_newwithsrc_b7938f86f737fe1d = handleError(function(arg0, arg1) {
+        var ret = new Audio(getStringFromWasm0(arg0, arg1));
+        return addHeapObject(ret);
+    });
     imports.wbg.__wbg_call_20c04382b27a4486 = handleError(function(arg0, arg1) {
         var ret = getObject(arg0).call(getObject(arg1));
         return addHeapObject(ret);
@@ -177,11 +366,22 @@ async function init(input) {
         var ret = getObject(arg0) === undefined;
         return ret;
     };
+    imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
+        var ret = debugString(getObject(arg1));
+        var ptr0 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len0 = WASM_VECTOR_LEN;
+        getInt32Memory0()[arg0 / 4 + 1] = len0;
+        getInt32Memory0()[arg0 / 4 + 0] = ptr0;
+    };
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
     imports.wbg.__wbindgen_rethrow = function(arg0) {
         throw takeObject(arg0);
+    };
+    imports.wbg.__wbindgen_closure_wrapper34 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 3, __wbg_adapter_18);
+        return addHeapObject(ret);
     };
 
     if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate web_sys;
 mod livro1;
 mod livro2;
 mod livro3;
+mod sons;
 
 use wasm_bindgen::prelude::*;
 use web_sys::*;
@@ -11,6 +12,19 @@ use wasm_bindgen::JsCast;
 use livro1::LIVRO_1;
 use livro2::LIVRO_2;
 use livro3::LIVRO_3;
+use sons:: { SOM_INICIO, SOM_TERMINO };
+
+pub struct IntervalHandle {
+    interval_id: i32,
+    _closure: Closure<dyn FnMut()>,
+}
+
+impl Drop for IntervalHandle {
+    fn drop(&mut self) {
+        let window = web_sys::window().unwrap();
+        window.clear_interval_with_handle(self.interval_id);
+    }
+}
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
@@ -19,49 +33,109 @@ pub fn main() -> Result<(), JsValue> {
     let document = window.document().expect("should have a document on window");
     let escolher = document.get_element_by_id("escolher").unwrap().dyn_into::<web_sys::HtmlElement>()?;
     let pista = document.get_element_by_id("pista").unwrap().dyn_into::<web_sys::HtmlElement>()?;
+    let ver_pista = document.get_element_by_id("verPista").unwrap().dyn_into::<web_sys::HtmlElement>()?;
     escolher.set_hidden(false);
     pista.set_hidden(true);
+
+    let btn_click = Closure::wrap(Box::new(move || {
+        usar_pista().expect("Não fez botão");
+    }) as Box<dyn FnMut()>);
+    ver_pista.set_onclick(Some(btn_click.as_ref().unchecked_ref()));
+    btn_click.forget();
+
     Ok(())
 }
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+pub fn usar_pista() -> Result<(), JsValue> {
+    let window = window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let texto_pista = document.get_element_by_id("textoPista").unwrap().dyn_into::<web_sys::HtmlElement>()?;
+    let numero_pista = document.get_element_by_id("numeroPista").unwrap().dyn_into::<web_sys::HtmlInputElement>()?;
+    let livro1 = document.get_element_by_id("livro1").unwrap().dyn_into::<web_sys::HtmlInputElement>()?;
+    let livro2 = document.get_element_by_id("livro2").unwrap().dyn_into::<web_sys::HtmlInputElement>()?;
+    let livro3 = document.get_element_by_id("livro3").unwrap().dyn_into::<web_sys::HtmlInputElement>()?;
 
-    #[wasm_bindgen(js_namespace = console)]
-    pub fn log(s: &str);
+    let numero_pista_value = numero_pista.value();
+
+    match numero_pista_value.trim().parse::<usize>() {
+        Ok(result) => {
+            if livro1.checked() {
+                if result > 0 && result <= LIVRO_1.len(){
+                    texto_pista.set_inner_text(LIVRO_1[result - 1]);
+                    carregar_pista()?;
+                }
+                else {
+                    window.alert_with_message(&format!("Escolha um número entre 1 e {}.", LIVRO_1.len()))?;
+                }
+            }
+            if livro2.checked() {
+                if result > 0 && result <= LIVRO_2.len(){
+                    texto_pista.set_inner_text(LIVRO_2[result - 1]);
+                    carregar_pista()?;
+                }
+                else {
+                    window.alert_with_message(&format!("Escolha um número entre 1 e {}.", LIVRO_2.len()))?;
+                }
+            }
+            if livro3.checked() {
+                if result > 0 && result <= LIVRO_3.len(){
+                    texto_pista.set_inner_text(LIVRO_3[result - 1]);
+                    carregar_pista()?;
+                }
+                else {
+                    window.alert_with_message(&format!("Escolha um número entre 1 e {}.", LIVRO_3.len()))?;
+                }
+            }
+        }
+        Err(_) => {
+            window.alert_with_message("Digite um numero no campo.")?;
+        }
+    }
+
+    Ok(())
 }
 
-#[wasm_bindgen]
-pub fn usar_pista(numero_livro: u8, numero_pista: usize) -> Result<(), JsValue> {
+pub fn carregar_pista() -> Result<(), JsValue> {
+    let som_inicio = HtmlAudioElement::new_with_src(&format!("data:audio/wav;base64,{}", SOM_INICIO));
     let window = window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let escolher = document.get_element_by_id("escolher").unwrap().dyn_into::<web_sys::HtmlElement>()?;
     let pista = document.get_element_by_id("pista").unwrap().dyn_into::<web_sys::HtmlElement>()?;
-    // let body = document.body().expect("document should have a body");
 
-    let texto_pista = document.get_element_by_id("textoPista").unwrap().dyn_into::<web_sys::HtmlElement>()?;
-    if numero_livro == 1 {
-        texto_pista.set_inner_text(LIVRO_1[numero_pista - 1]);
-    }
-    if numero_livro == 2 {
-        texto_pista.set_inner_text(LIVRO_2[numero_pista - 1]);
-    }
-    if numero_livro == 3 {
-        texto_pista.set_inner_text(LIVRO_3[numero_pista - 1]);
-    }
-
+    let _ = som_inicio.unwrap().play().unwrap();
     atualizar_tempo_restante(30)?;
-    // Thread não funciona com wasm
-    // thread::sleep(time::Duration::from_secs(1));
-
     pista.set_hidden(false);
     escolher.set_hidden(true);
+    atualizar()?;
 
     Ok(())
 }
 
-#[wasm_bindgen]
+pub fn atualizar() -> Result<(), JsValue> {
+    let som_termino = HtmlAudioElement::new_with_src(&format!("data:audio/wav;base64,{}", SOM_TERMINO));
+    let window = window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let escolher = document.get_element_by_id("escolher").unwrap().dyn_into::<web_sys::HtmlElement>()?;
+    let pista = document.get_element_by_id("pista").unwrap().dyn_into::<web_sys::HtmlElement>()?;
+
+    let tempo = pegar_tempo_restante().unwrap();
+
+    if tempo <= 0 {
+        let _ = som_termino.unwrap().play().unwrap();
+        pista.set_hidden(true);
+        escolher.set_hidden(false);
+    } else {
+        atualizar_tempo_restante(tempo - 1)?;
+        let contagem = Closure::wrap(Box::new(|| {
+            atualizar().unwrap();
+        }) as Box<dyn FnMut()>);
+        window.set_timeout_with_callback_and_timeout_and_arguments_0(contagem.as_ref().unchecked_ref(), 900)?;
+        contagem.forget();
+    }
+
+    Ok(())
+}
+
 pub fn atualizar_tempo_restante (tempo_restante: i8) -> Result<(), JsValue> {
     let window = window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
@@ -69,4 +143,19 @@ pub fn atualizar_tempo_restante (tempo_restante: i8) -> Result<(), JsValue> {
     
     texto_pista.set_inner_text(&format!("{}", tempo_restante));
     Ok(())
+}
+
+pub fn pegar_tempo_restante() -> Result<i8, JsValue> {
+    let window = window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let tempo_restante = document.get_element_by_id("tempoRestante").unwrap().dyn_into::<web_sys::HtmlElement>()?;
+    match tempo_restante.inner_text().trim().parse::<i8>() {
+        Ok(tempo) => {
+            Ok(tempo)
+        }
+        Err(error) => {
+            window.alert_with_message(&format!("{}", error))?;
+            Ok(1)
+        }
+    }
 }
